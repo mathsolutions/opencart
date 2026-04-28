124a87cad7a9faf70a4ca227ff52f2d2f8bfe05a
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-04-28 22:26:30 +0800

    Update special.js

--- upload/catalog/view/javascript/catalog/special.js
+++ upload/catalog/view/javascript/catalog/special.js
@@ -1,8 +1,15 @@
 import { Controller } from '../component.js';
 import { loader } from '../index.js';
 
+// Language
+const language = await loader.language('catalog/special');
+
 export default class extends Controller {
     async connected() {
 
+    }
+
+    render() {
+        return loader.template('catalog/special', { ...language });
     }
 }
