de98f9afb666364f52779e374b51bd73481855e5
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-04-28 22:26:02 +0800

    Update newsletter.js

--- upload/catalog/view/javascript/account/newsletter.js
+++ upload/catalog/view/javascript/account/newsletter.js
@@ -2,20 +2,20 @@ import { Controller } from '../component.js';
 import { loader } from '../index.js';
 
 // Language
-const language = await loader.language('account/edit');
+const language = await loader.language('account/newsletter');
 
 // Library
-const session = loader.library('session');
+const session = await loader.library('session');
 
 export default class extends Controller {
     async render() {
         let data = {};
 
-        let customer = session.get('customer');
+        //let customer = session.get('customer');
 
-        data.newsletter = customer.get('newsletter');
+        //data.newsletter = customer.get('newsletter');
 
-        return loader.template('account/newsletter', { ...data, ...language, ...config });
+        return await loader.template('account/newsletter', { ...data, ...language });
     }
 
     onSubmit(e) {
